8d9cabbe309269039a34fdfb46d5c64581ce3ff8
kamin <dev.kaminlim@gmail.com>
2022-01-06 18:49:09 +0900

    solve

--- matrix/src/main/kotlin/Matrix.kt
+++ matrix/src/main/kotlin/Matrix.kt
@@ -1,10 +1,31 @@
-class Matrix(private val matrixAsString: String) {
+class Matrix(matrixAsString: String) {
+
+    private val matrix : List<List<Int>>
+
+    init {
+        val v1 = matrixAsString.split("\n")
+        val v2 = mutableListOf<List<Int>>()
+
+
+        for (line in v1) {
+            //정규식
+            val v3 = line.split(" ").filter { it.isNotEmpty() }
+            val v4 = mutableListOf<Int>()
+            for (number in v3) {
+                v4.add(number.toInt())
+            }
+            v2.add(v4)
+
+        }
+        this.matrix = v2
+        println(this.matrix)
+    }
 
     fun column(colNr: Int): List<Int> {
-        TODO("Implement this to complete the task")
+        return matrix.map { it[colNr - 1] }
     }
 
     fun row(rowNr: Int): List<Int> {
-        TODO("Implement this to complete the task")
+        return matrix[rowNr-1]
     }
 }
