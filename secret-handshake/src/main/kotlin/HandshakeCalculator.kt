62d73e1ecdde5b6801ee6410705393a53403cdab
kamin <dev.kaminlim@gmail.com>
2022-01-08 04:23:41 +0900

    solve

--- secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -1,5 +1,23 @@
 object HandshakeCalculator {
     fun calculateHandshake(number: Int): List<Signal> {
-        TODO("Implement the function to complete the task")
+        val result = mutableListOf<Signal>()
+
+        if (number % 2 == 1) {
+            result.add(Signal.WINK)
+        }
+        if (number % 4 > 1) {
+            result.add(Signal.DOUBLE_BLINK)
+        }
+        if (number % 8 > 3) {
+            result.add(Signal.CLOSE_YOUR_EYES)
+        }
+        if (number % 16 > 7) {
+            result.add(Signal.JUMP)
+        }
+        if (number % 32 > 15) {
+            return result.reversed()
+        }
+
+        return result;
     }
 }
