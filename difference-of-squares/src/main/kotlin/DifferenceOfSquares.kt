7ec15698141727ca835636dbf82e7791a306bf79
kamin <dev.kaminlim@gmail.com>
2022-01-08 18:25:06 +0900

    update

--- difference-of-squares/src/main/kotlin/DifferenceOfSquares.kt
+++ difference-of-squares/src/main/kotlin/DifferenceOfSquares.kt
@@ -1,12 +1,11 @@
-class Squares (var n: Int){
+data class Squares(val n: Int) {
 
     fun sumOfSquares(): Int {
-        return (1..n).sumOf { pow(it, 2) }
+        return n * (n + 1) * (2 * n + 1) / 6
     }
 
     fun squareOfSum(): Int {
-        val number = (1..n).fold(0) { acc, number -> acc + number }
-        return pow(number, 2)
+        return pow(n * (n + 1) / 2, 2)
     }
 
     fun difference(): Int {
@@ -14,34 +13,20 @@ class Squares (var n: Int){
     }
 
     fun pow(base: Int, exp: Int): Int {
-        if (exp < 0) {
-            return 0
-        } else if (exp < 1) {
-            return 1
-        } else {
-            var result = base
-            var e = 1
-            if (e < exp) {
-                result *= base
-                e += 1
-            }
-            while (e < exp) {
-                if (exp / 2 < e) {
-                    result *= base
-                    e += 1
-                } else {
-                    result *= result
-                    e *= 2
-                }
-            }
-            return result
-        }
+        return pow(base, exp, 1)
     }
-}
 
-fun main() {
-    val s = Squares(1)
-    for (i in -10..10) {
-        println("$i ${s.pow(2, i)}")
+    tailrec fun pow(base: Int, exp: Int, multiplier: Int): Int {
+        if (0 < exp) {
+            return when (exp % 2) {
+                0 -> pow(base * base, exp / 2, multiplier)
+                else -> pow(base, exp - 1, multiplier * base)
+            }
+        } else {
+            return when (exp) {
+                0 -> multiplier
+                else -> 0
+            }
+        }
     }
 }
