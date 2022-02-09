d29c11df82e4f6eddd3c9a6aa55575e649f7dad8
kamin <dev.kaminlim@gmail.com>
2022-02-09 19:55:54 +0900

    solve

--- word-count/src/main/kotlin/WordCount.kt
+++ word-count/src/main/kotlin/WordCount.kt
@@ -1,6 +1,14 @@
 object WordCount {
 
     fun phrase(phrase: String): Map<String, Int> {
-        TODO("Implement this function to complete the task")
+        val v1 = phrase.toLowerCase()
+                .split(Regex("[^a-z'\\d]+"))
+
+        var v2 = v1.filterNot { it.isEmpty() }
+                .map { it.trim('\'') }
+                .groupingBy { it }.eachCount() as HashMap<String, Int>
+        return v2
     }
 }
+
+
