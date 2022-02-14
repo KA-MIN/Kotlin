197a88f059c52c84b8492ac47505516e4d0f0cb4
kamin <dev.kaminlim@gmail.com>
2022-02-15 01:05:02 +0900

    solve

--- robot-name/src/main/kotlin/RobotName.kt
+++ robot-name/src/main/kotlin/RobotName.kt
@@ -1,9 +1,20 @@
+import java.util.Random
+
 class Robot {
 
     val name: String
-        get() = TODO("Implement this getter to complete the task")
+        get() = randomName()
 
-    fun reset() {
-        TODO("Implement this function to complete the task")
+    fun reset() = randomName()
+
+    fun randomName(): String {
+        var random = Random()
+        var temp = 'A' + random.nextInt('Z' - 'A')
+        var temp2 = 'A' + random.nextInt('Z' - 'A')
+        var number = random.nextInt((999-100)+1)
+
+        var result = temp.plus(temp2.toString()).plus(number)
+
+        return result
     }
 }
